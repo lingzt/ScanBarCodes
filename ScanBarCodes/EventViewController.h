5c3fe4aa063005972b8d14dfdbb9739a54cc73ee
Ling <asklingtoby@gmail.com>
2016-07-27 18:38:30 -0400

    build QRCode scanner

---
+++ ScanBarCodes/EventViewController.h
@@ -0,0 +1,14 @@
+//
+//  EventViewController.h
+//  ScanBarCodes
+//
+//  Created by ling toby on 7/27/16.
+//  Copyright © 2016 Infragistics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+@property (strong,nonatomic) NSString* scannedResult;
+
+@end
